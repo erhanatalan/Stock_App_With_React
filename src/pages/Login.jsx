6ee15b768d0ad5a78731e8ba1aa0c4d3a0d11967
erhanatalan <erhanatalan2013@gmail.com>
2023-03-28 15:41:51 +0300

    login  input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,12 +6,19 @@ import Typography from "@mui/material/Typography";
 import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/result.svg";
 import { Link, useNavigate } from "react-router-dom";
-
+import { Formik, Form } from "formik";
 import { useSelector } from "react-redux";
+import TextField from "@mui/material/TextField";
+import { object, string, number, date, InferType } from "yup";
 
 const Login = () => {
   const navigate = useNavigate();
   const { currentUser, error } = useSelector((state) => state?.auth);
+
+  const loginSchema = object({
+    email: string().email("Lutfen dogru bir email giriniz.").required("Bu alan zorunludur."),
+    password: string().required("Password zorunludur").min(8,"Minumum 8 karakter olmalidir.").max(16, "maximum 16 karakter olmalidir.").matches(/\d+/, "Password en az bir sayi icermelidir.").matches(/[a-z]/, "Password en az bir kucuk harf icermelidir.").matches(/[A-Z]/, "Password en az bir buyuk harf icermelidir."),
+  });
 
   return (
     <Container maxWidth="lg">
@@ -49,7 +56,61 @@ const Login = () => {
           >
             Login
           </Typography>
-
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={loginSchema}
+            onSubmit={(values, actions) => {
+              //todo post
+              navigate("/stock");
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <Form onSubmit={handleSubmit}>
+                <Box
+                  sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
+                >
+                  <TextField
+                    label="Email"
+                    type="email"
+                    id="email"
+                    name="email"
+                    variant="outlined"
+                    error={touched?.email && Boolean(errors?.email)}
+                    helperText={touched?.email && errors?.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values?.email || ""}
+                  />
+                  <TextField
+                    label="Password"
+                    type="password"
+                    name="password"
+                    variant="outlined"
+                    error={touched?.password && Boolean(errors?.password)}
+                    helperText={touched?.password && errors?.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                  />
+                </Box>
+                {errors.password && touched.password && errors.password}
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
           </Box>
