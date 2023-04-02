5392ccb0b915001da6d10e9461a3ca82e180f420
erhanatalan <erhanatalan2013@gmail.com>
2023-04-02 11:56:17 +0300

    drawer finish

--- src/components/MenuListItems.jsx
+++ src/components/MenuListItems.jsx
@@ -3,41 +3,77 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import List from '@mui/material/List';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import StoreIcon from '@mui/icons-material/Store';
+import StarsIcon from '@mui/icons-material/Stars';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import { useNavigate } from 'react-router-dom';
 
 const icons = [
     {
-        
-    }
+        title: "Dashboard",
+        icon: <DashboardIcon/>,
+        url: "/stock/",
+    },
+    {
+        title: "Purchase",
+        icon: <ShoppingCartIcon/>,
+        url: "/stock/purchase/",
+    },
+    {
+        title: "Sales",
+        icon: <AttachMoneyIcon/>,
+        url: "/stock/sales/",
+    },
+    {
+        title: "Firms",
+        icon: <StoreIcon/>,
+        url: "/stock/firms/",
+    },
+    {
+        title: "Brands",
+        icon: <StarsIcon/>,
+        url: "/stock/brands/",
+    },
+    {
+        title: "Products",
+        icon: <InventoryIcon/>,
+        url: "/stock/products/",
+    },
+    {
+        title: "Admin Panel",
+        icon: <SupervisorAccountIcon/>,
+        url: "https://12226.fullstack.clarusway.com/admin/",
+    },
 ]
 
-const MenuListItems = ({open}) => {
-
+const MenuListItems = () => {
+  const navigate = useNavigate()
   return (
     <div>
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          {icons?.map((item, index) => (
+        
+            <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+              {item.url.includes("http") ? (
+              <ListItemButton to={item.url}>
+                <ListItemIcon>
+                  {item.icon}
                 </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
+                <ListItemText primary={item.title} />
+              </ListItemButton>) 
+              :(
+                <ListItemButton onClick={()=> navigate(item.url)}>
+                <ListItemIcon>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.title} />
+              </ListItemButton>)}
+              
             </ListItem>
           ))}
         </List>
